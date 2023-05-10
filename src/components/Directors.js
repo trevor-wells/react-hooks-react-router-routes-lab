fed650a778ac4c1e8df7fda8e9cdfc8ab4466711
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-10 11:49:13 -0600

    almost there

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,19 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
+  const directorList = directors.map(director => 
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  )
+
   return (
     <div>
-      
+      <h1>Directors Page</h1>
+      {directorList}
     </div>)
 }
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,11 +3,11 @@ import { movies } from "../data";
 
 function Movies() {
   const movieList = movies.map(movie =>
-    <div>
+    <div key={movie.title}>
       <h2>{movie.title}</h2>
       <h3>{movie.time} minutes</h3>
       <ul>
-        {movie.genres.map(genre => <li>{genre}</li>)}
+        {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
       </ul>
     </div>
   )
