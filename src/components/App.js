2ebdc5324401382d42bb9866511d5f5bc83718b7
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-10 11:51:31 -0600

    wtf

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,26 @@ import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
-function App() {
-  return(
-  <div>
-    <NavBar />
-    <Switch>
-      <Route exact path = "/">
-        <Home />
-      </Route>
-      <Route exact path = "/movies">
-        <Movies />
-      </Route>
-      <Route exact path = "/directors">
-        <Directors />
-      </Route>
-      <Route exact path = "/actors">
-        <Actors />
-      </Route>
-    </Switch>
-  </div>
-  )
-}
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/directors">
+          <Directors />
+        </Route>
+        <Route path="/actors">
+          <Actors />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
